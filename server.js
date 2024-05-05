a6e243107cd653dd17b38261c1b25f578dbcafe5
anurag <champhysics@gmail.com>
2024-05-05 16:53:29 +0530

    depolyment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import authRoutes from './routes/authRoute.js';
-import connectDB from './config/db.js';
-import cors from 'cors'
-import categoryRoutes from './routes/categoryRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import authRoutes from "./routes/authRoute.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
 const app = express();
 // Configure environment variables
 dotenv.config();
@@ -13,25 +14,23 @@ dotenv.config();
 // Connect to the database
 connectDB();
 
-
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 // Routes
-app.use('/api/v1/auth', authRoutes);
-app.use("/api/v1/category",categoryRoutes)
-app.use('/api/v1/product',productRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome to ecommerce app</h1>")
-})
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
